a8df2b6d1c4c8ad5de5cd6e465114aafd872e399
Thalisson Vieira <tdevieira@gmail.com>
2025-09-09 22:19:22 -0300

    Implementação de CRUD usando Spring Boot

--- produtosapi/src/main/java/spring_boot_specialization/produtosapi/controller/ProdutoController.java
+++ produtosapi/src/main/java/spring_boot_specialization/produtosapi/controller/ProdutoController.java
@@ -1,15 +1,24 @@
 package spring_boot_specialization.produtosapi.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import spring_boot_specialization.produtosapi.model.Produto;
 import spring_boot_specialization.produtosapi.repository.ProdutoRepository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
+/*
+@RestController marca a classe como um controlador REST. Ele combina:
+@Controller (registra a classe como controlador MVC) +
+@ResponseBody (retorno dos métodos e vai para o corpo da resposta — normalmente JSON)
+ */
 @RestController
+/*
+@RequestMapping: Anotação do Spring MVC para mapear rotas HTTP.
+Define rota específica e condições (método HTTP, consumes, produces, params, headers etc.).
+Usar em  mapeamento CRUD (@GetMapping, @PostMapping, @PutMapping, @DeleteMapping, @PatchMapping).
+ */
 @RequestMapping("/produtos")
 public class ProdutoController {
 
@@ -27,4 +36,26 @@ public class ProdutoController {
         produtoRepository.save(produto);
         return produto;
     }
+
+    @GetMapping("/obter/{id}")
+    public Produto obterPorId(@PathVariable  String id) {
+        return produtoRepository.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("/apagar/{id}")
+    public String apagarPorId(@PathVariable String id) {
+        produtoRepository.deleteById(id);
+        return String.format("Produto %s apagado.", id);
+    }
+
+    @PutMapping("/editar/{id}")
+    public Produto editarPorId(@PathVariable String id, @RequestBody Produto produto){
+        Optional<Produto> byId = produtoRepository.findById(id);
+        Produto produtoN = byId.orElseGet(Produto::new);
+        produtoN.setNome(produto.getNome());
+        produtoN.setDescricao(produto.getDescricao());
+        produtoN.setPreco(produto.getPreco());
+        return produtoRepository.save(produtoN);
+    }
+
 }
